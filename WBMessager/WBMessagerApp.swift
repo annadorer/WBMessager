510698376be6177890c5dac407ce0089f1048ea5
Anna Plotnikova <baebaeannieee@gmail.com>
2024-06-06 16:42:21 +0700

    Initial Commit

---
+++ WBMessager/WBMessagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBMessagerApp.swift
+//  WBMessager
+//
+//  Created by Anna on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBMessagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
